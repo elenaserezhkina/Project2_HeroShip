42806fbee37f97198de14541bdc75048b8407c8e
Maja Gach <majaanna94@gmail.com>
2020-03-05 14:50:54 +0100

    added header test

---
+++ src/Components/Header/HeaderNavigation.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./HeaderNavigation.scss";
+
+function HeaderNavigation() {
+  return <div className="header-container">hej!</div>;
+}
+
+export default HeaderNavigation;
