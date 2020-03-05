e77c093e68389f0730e4ade273917798d3a730c5
Maja Gach <majaanna94@gmail.com>
2020-03-05 15:00:31 +0100

    added header with logo

--- src/Components/Header/HeaderNavigation.jsx
+++ src/Components/Header/HeaderNavigation.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./HeaderNavigation.scss";
+import Logo from "../../Pictures/logo.svg";
 
 function HeaderNavigation() {
-  return <div className="header-container">hej!</div>;
+  return (
+    <div className="header-container">
+      <img src={Logo} alt="logo" className="header-logo" />
+    </div>
+  );
 }
 
 export default HeaderNavigation;
