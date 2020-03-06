6633528630885cd5c10e9d4158ebc7105b9014ab
Maja Gach <majaanna94@gmail.com>
2020-03-06 12:12:11 +0100

    added details to side B

--- src/Components/HeroInfo/SideB.jsx
+++ src/Components/HeroInfo/SideB.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./SideB.scss";
 
-function SideB({ changeCard }) {
+function SideB({ changeCard, hero }) {
+  console.log(hero);
   return (
     <div>
       <div className="wrapper-B">
@@ -10,14 +11,13 @@ function SideB({ changeCard }) {
             <div className="hero-picture-wrapper">
               <div className="card-with-details">
                 <ul className="characteristic">
-                  <li>meet Name, :</li>
+                  <li>meet {hero.name}, :</li>
                   <li>also known as Alter-egos: </li>
-                  <li>Mostly works at Occupation : </li>
-                  <li>Deffenetly on the Alignment side. </li>
-                  <li> Intelligence : </li>
-                  <li> Power : </li>
-                  <li>Eye color : </li>
-                  <li> Hair color : </li>
+                  <li>Mostly works as {hero.work.occupation}. </li>
+                  <li>Deffenetly on the {hero.biography.alignment} side. </li>
+                  <li> Intelligence : {hero.powerstats.intelligence} </li>
+                  <li> Power : {hero.powerstats.power} </li>
+                  {/* <li>Eye color : {hero.appearance.["eye-contact"]}</li> */}
                 </ul>
                 <button onClick={changeCard} className="button-goback">
                   {" "}
